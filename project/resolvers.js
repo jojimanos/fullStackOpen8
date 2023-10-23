0764b74e5682faf5abfb191dbbea6f5be39d4787
jojimanos <jojimanos@192.168.1.8>
2023-10-23 03:00:44 -0400

    refactoring the backend

---
+++ project/resolvers.js
@@ -0,0 +1,141 @@
+const { GraphQLError } = require('graphql')
+const Author = require('./schema/author')
+const Book = require('./schema/book')
+const User = require('./schema/user')
+const { v1: uuid } = require('uuid')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+const resolvers = {
+  Query: {
+    bookCount: async () => Book.collection.countDocuments(),
+    authorCount: async () => Author.collection.countDocuments(),
+    allBooks: async (root, args) => {
+      try {
+        let result
+        if (!args.author && !args.genres) {
+          result = Book.find({}).populate('author')
+        }
+        else if (!args.genres) {
+
+          let author = await Author.findOne({ name: args.author })
+
+          console.log(author)
+
+          let books = Book.find({ author: author._id }).populate('author')
+          result = books
+        }
+        else if (!args.author) {
+          let books = Book.find({ genres: args.genres }).populate('author')
+          result = books
+        }
+        else if (args.author && args.genres) {
+
+          let author = await Author.findOne({ name: args.author }).populate('author')
+
+          let books = Book.find({ author: author._id, genres: args.genres })
+          result = books
+        }
+        return result
+      } catch (error) {
+        throw new GraphQLError(`Failed to perform the search: ${error.message}`)
+      }
+    },
+    allAuthors: async () => Author.find({}),
+    me: (root, args, context) => {
+      return context.currentUser
+    }
+  },
+  Author: {
+    name: (root) => root.name,
+    id: (root) => root.id,
+    bookCount: (root) => {
+      let count = Book.collection.countDocuments({ author: root._id })
+      return count
+    }
+  },
+  Mutation: {
+    addBook: async (root, args) => {
+      try {
+        // Check if the author already exists in the database
+        let author = await Author.findOne({ name: args.author });
+
+        // If the author doesn't exist, create a new author
+        if (!author) {
+          author = new Author({ name: args.author });
+          await author.save();
+        }
+
+        console.log(author)
+
+        // Create a new book using the provided data and the author's ObjectId
+        const book = new Book({
+          title: args.title,
+          published: args.published,
+          genres: args.genres,
+          author: author
+        });
+
+        await book.save();
+        return book;
+      } catch (error) {
+        throw new GraphQLError(`Failed to add a book: ${error.message}`);
+      }
+    },
+    editAuthor: async (root, args) => {
+      try {
+        const author = await Author.findOne({ name: args.name })
+        author.born = args.setBornTo
+        return author.save()
+      } catch (error) {
+        throw new GraphQLError(`There was an error while ${error.message}`)
+      }
+    },
+    createUser: async (root, args) => {
+
+      try {
+
+        let saltRounds = 10
+
+        const salt = bcrypt.genSaltSync(saltRounds);
+        const hash = bcrypt.hashSync(args.password, salt)
+
+        const user = new User({ username: args.username, password: hash, favouriteGenre: args.favouriteGenre })
+        return user.save()
+      } catch (error) {
+        throw new GraphQLError(`Creating the user failed ${error.message}`, {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.username,
+            error
+          }
+        })
+      }
+    },
+    login: async (root, args) => {
+
+      const user = await User.findOne({ username: args.username })
+
+      const passwordCorrect = user === null
+        ? false
+        : await bcrypt.compare(args.password, user.password)
+
+      if (!(user && passwordCorrect)) {
+        throw new GraphQLError('wrong credentials', {
+          extensions: {
+            code: 'BAD_USER_INPUT'
+          }
+        })
+      }
+
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
+    },
+  }
+}
+
+module.exports = resolvers
---
+++ project/schema.js
@@ -0,0 +1,59 @@
+const typeDefs = `
+type Query {
+  bookCount: Int!
+  authorCount: Int!
+  allBooks(author: String, genres: [String]): [Book!]!
+  allAuthors: [Author!]!
+  me: User
+  }
+
+  type User {
+    username: String!
+    password: String!
+    favouriteGenre: String!
+    id: ID!
+  }
+
+  type Token {
+    value: String!
+  }
+
+type Author {
+  name: String!
+  id: String!
+  born: Int
+  bookCount: Int
+}
+
+type Book {
+  title: String!
+  published: Int!
+  author: Author!
+  id: String!
+  genres: [String!]!
+}
+
+type Mutation {
+  addBook(
+    title: String!
+    author: String!
+    published: Int!
+    genres: [String!]!
+  ): Book
+  editAuthor(
+    name: String!
+    setBornTo: Int!
+  ): Author
+  createUser(
+    username: String!
+    password: String!
+    favouriteGenre: String!
+  ): User
+  login(
+    username: String!
+    password: String!
+  ): Token
+}
+`
+
+module.exports = typeDefs
