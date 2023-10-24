28e1e373fb8da775d186aa93e9186880da135a0d
jojimanos <jojimanos@192.168.1.8>
2023-10-24 09:28:41 +0300

    refactoring the backend using express and cors

--- project/index.js
+++ project/index.js
@@ -1,18 +1,31 @@
+const { ApolloServer } = require('@apollo/server')
+
+const { expressMiddleware } = require('@apollo/server/express4')
+const { ApolloServerPluginDrainHttpServer } = require('@apollo/server/plugin/drainHttpServer')
+const { makeExecutableSchema } = require('@graphql-tools/schema')
+const express = require('express')
+const cors = require('cors')
+const http = require('http')
+
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', false)
 
 const User = require('./schema/user')
 
+const typeDefs = require('./schema')
+const resolvers = require('./resolvers')
+
 require('dotenv').config()
 
-const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
 
 const jwt = require('jsonwebtoken')
+const { hostname } = require('os')
 
 const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect(MONGO_URI)
+mongoose
+  .connect(MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB')
   })
@@ -20,24 +33,43 @@ mongoose.connect(MONGO_URI)
     console.log("There was an error", error.message)
   })
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-})
+const start = async () => {
+  const app = express()
+  const httpServer = http.createServer(app)
 
-startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => {
-    const auth = req ? req.headers.authorization : null
-    if (auth && auth.startsWith('Bearer ')) {
-      const decodedToken = jwt.verify(
-        auth.substring(7), process.env.JWT_SECRET
-      )
-      const currentUser = await User
-        .findById(decodedToken.id)
-      return { currentUser }
-    }
-  },
-}).then(({ url }) => {
-  console.log(`Server ready at ${url}`)
-})
+  const server = new ApolloServer({
+    schema: makeExecutableSchema({ typeDefs, resolvers }),
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+
+  })
+
+  await server.start()
+
+  app.use(
+    '/',
+    cors(),
+    express.json(),
+    expressMiddleware(server, {
+      context: async ({ req, res }) => {
+        const auth = req ? req.headers.authorization : null
+        if (auth && auth.startsWith('Bearer ')) {
+          const decodedToken = jwt.verify(
+            auth.substring(7), process.env.JWT_SECRET
+          )
+          const currentUser = await User
+            .findById(decodedToken.id)
+          return { currentUser }
+        }
+      },
+    })
+  )
+  const PORT = 4000
+
+  httpServer.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  }
+  )
+}
+
+
+start()
